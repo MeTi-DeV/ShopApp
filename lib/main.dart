184fd4a9f2f029236244205a8539acb42e4e7481
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-24 18:58:33 +0330

    initial Shop App Flutter

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      title: 'My Shop',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a new shop'),
+      ),
+    );
+  }
+}
