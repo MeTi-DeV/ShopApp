696cc644bb495957c77896f3cdf27140f2d60da7
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-26 20:46:43 +0330

    create overview product screen and defi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './screens/overview_products_screen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,23 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue,
+      fontFamily: 'Lato',
+      accentColor: Colors.orangeAccent
+      ),
       debugShowMaterialGrid: false,
       title: 'My Shop',
-      home: MyHomePage(),
+      home: OverviewProductsScreen()
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Shop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a new shop'),
-      ),
-    );
-  }
-}
